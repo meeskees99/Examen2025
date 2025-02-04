489a4e307600eb001080f9d9c07024f299c0e639
LusseveldNiek <schoolwerknieklusseveld@gmail.com>
2025-02-04 11:05:56 +0100

    Begin aan RotationManager

--- Assets/Scripts/RotationManager.cs
+++ Assets/Scripts/RotationManager.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class RotationManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /* Ik gebruik hier een enum om de huidige status bij te houden van de rotation manager,
+    zodat ik duidelijk in stapjes het process kan coderen */
+    public enum RotationState
+    {
+        NotActive,
+        Rotating
+    }
+
+    // -------- VARIABLES --------
+
+    [Header("Values")]
+    [SerializeField]
+    private float _rotationSpeed;
+    [SerializeField]
+    private float _tiltSpeed;
+
+    private float _currentMouseSpeed;
+    private Vector3 _currentMousePosition;
+
+    [Header("Objects")]
+    public Camera CameraToRotate;
+    public Transform TransformToLookAt; // position waar de camera altijd naar kijkt
+    public GameObject Prefab;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // functie om de camera te roteren
+    void RotateCamera()
+    {
+
+    }
+
+    // functie om de camera te tilten
+    void TiltCamera()
+    {
+
+    }
+
+    // calculeer hier de snelheid van de muis van de user
+    void CalculateMouseSpeed(Vector3 mousePosition)
+    {
+
     }
 }
