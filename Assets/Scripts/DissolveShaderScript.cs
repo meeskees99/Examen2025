4a2e7ecef216b564cb86b56f2d537a00bc4a8655
SpookyWizard27 <97090185@st.deltion.nl>
2025-02-07 14:19:01 +0100

    Shaders
    Shaders aan en uit button aangemaakt. Hij kan nu de shader pauzeren.

--- Assets/Scripts/DissolveShaderScript.cs
+++ Assets/Scripts/DissolveShaderScript.cs
@@ -6,32 +6,41 @@ public class DissolveShaderScript : MonoBehaviour
 {
 
     public Material dissolveMaterial;
-    public float dissolveslider = -1f;
-    public float speed = 0.05f;
+    public float dissolveslider;
+    public float speed = 0.2f;
     public bool on;
+    public bool done;
 
     // Start is called before the first frame update
     void Start()
     {
         dissolveMaterial.SetFloat("_Time_Control", dissolveslider);
-
+        //dissolveslider = -1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        dissolveslider = Mathf.Clamp(dissolveslider, -1f, 1f);
-
-        if (on == true && dissolveMaterial.GetFloat("_Time_Control") < 1f)
+        if (done == false)
         {
-            dissolveslider += speed * Time.fixedDeltaTime;
-        }
-        else if (dissolveMaterial.GetFloat("_Time_Control") > -1f)
-        {
-            dissolveslider -= speed * Time.fixedDeltaTime;
-        }
+            dissolveslider = Mathf.Clamp(dissolveslider, -1f, 1f);
 
-        dissolveMaterial.SetFloat("_Time_Control", dissolveslider);
+            if (on == true && dissolveMaterial.GetFloat("_Time_Control") < 1f)
+            {
+                dissolveslider += speed * Time.deltaTime;
+            }
+            else if (dissolveMaterial.GetFloat("_Time_Control") > -1f)
+            {
+                dissolveslider -= speed * Time.deltaTime;
+            }
+
+            dissolveMaterial.SetFloat("_Time_Control", dissolveslider);
+        }
+        if(on == true && dissolveslider > 0.95f || on == false && dissolveslider < -0.95f)     //
+        {
+            done = true;
+        }
+        
     }
     public void DissolveOn()
     {
